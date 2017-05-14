9416ba7fbb76bf4fef884bc4aa0761e9a359c604
Beastie PC <gskondratenko@opora.org.ua>
2017-05-14 20:59:08 +0300

    fix poltergeist heroku

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,10 @@ require_relative './people'
 require 'capybara'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
-
+options = {js_errors: false}
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
 class ScrapeMp
   def parser
     i = 3000
