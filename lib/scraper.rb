d72f50afde63a549e51972686ecf48a956b8946c
Beastie PC <gskondratenko@opora.org.ua>
2017-08-30 14:07:34 +0300

    Added date end deputy and added resigned deputy

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,9 +19,28 @@ class ScrapeMp
       photo_url = "http://www.city.kharkov.ua" + get_page(mp.all('.lf_name a')[0][:href]).css('.box_photo img')[0][:src]
       faction = mp.find('.party a').text
       i = i + 1
-      scrape_mp(full_name, nil, faction, photo_url, i)
+      date_start = case
+                     when full_name == "Медведев Юрий Игоревич"
+                       "2016-05-18"
+                     when full_name == "Кривуля Виталий Евгеньевич"
+                       "2016-09-07"
+                     when full_name == "Китанин Виктор Александрович"
+                       "2016-10-18"
+                     when full_name == "Гунбина Елена Владимировна"
+                       "2017-01-16"
+                     else
+                       "2015-11-18"
+                   end
+      date_end = case
+                   when full_name == "Чепель Андрей Алексеевич"
+                     "2016-12-21"
+                   else
+                     "9999-12-31"
+                 end
+
+      scrape_mp(full_name, nil, faction, photo_url, i, date_start, date_end)
     end
-    #resigned_mp()
+    resigned_mp()
     create_mer()
   end
   def create_mer
@@ -36,7 +55,8 @@ class ScrapeMp
         okrug: nil,
         photo_url: "http://www.city.kharkov.ua/assets/images/kernes.jpg",
         faction: nil,
-        end_date: nil,
+        start_date: "2015-11-18" ,
+        end_date: "9999-12-31",
         created_at: "9999-12-31"
     )
   end
@@ -44,11 +64,15 @@ class ScrapeMp
     Nokogiri::HTML(open(url, "User-Agent" => "HTTP_USER_AGENT:Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.47"), nil, 'utf-8')
   end
   def resigned_mp
-    uri = ""
-    page_resigned = get_page(uri)
-    scrape_mp( )
+    mp = []
+    mp << {fio: "Лесик Андрей Анатольевич", okrug: nil, party: "За Харьков, за Возрождение", image: "http://lesik.kharkov.ua/images/lesik_andrey.png", rada_id: 2000 , date_start: "2015-11-18", date_end: "2016-05-17"}
+    mp << {fio: "Коринько Иван Васильевич", okrug: nil, party: "За Харьков, за Возрождение", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJpGOXa3AVhLBiRbf1q5dijotS5wQOQ6V1BqYjgRIPaEvorhQO06KFpJhl", rada_id: 2001 , date_start: "2015-11-18", date_end: "2016-06-30"}
+    mp << {fio: "Дегтярьов Николай Іванович", okrug: nil, party: "За Харьков, за Возрождение", image: "http://img3.sq.com.ua/image/88/133/img-articles-vip-dosie-degtiarev_n_i.jpg", rada_id: 2002 , date_start: "2015-11-18", date_end: "2016-09-14"}
+    mp.each do |m|
+      scrape_mp(m[:fio], m[:okrug], m[:party], m[:image], m[:rada_id], m[:date_start], m[:date_end])
+    end
   end
-  def scrape_mp(fio, okrug, party, image, rada_id ,date_end=nil)
+  def scrape_mp(fio, okrug, party, image, rada_id, date_start, date_end)
     party = case
               when party[/СОЛИДАРНОСТЬ/]
                 "Блок Петра Порошенка"
@@ -72,7 +96,7 @@ class ScrapeMp
         faction: party,
     )
     unless people.nil?
-    people.update(end_date:  date_end,  updated_at: Time.now)
+    people.update(end_date:  date_end, start_date: date_start, updated_at: Time.now)
     else
       People.create(
           first_name: name_array[1],
@@ -84,6 +108,7 @@ class ScrapeMp
           photo_url: image,
           faction: party,
           end_date:  date_end,
+          start_date: date_start,
           created_at: Time.now,
           updated_at: Time.now
       )
